06c5275fca2d049872ebfb7e2dc70f4a0d44378b
mmmmmrob <mmmmmrob@aa2d91ab-4d3b-0410-9a5e-bd65fa47c329>
2009-08-12 18:55:05 +0000

    refactored httprequest to introduce abstract httpclient and concrete phphttpclient and curlhttpclient impls - not yet tested

---
+++ branches/20090805_rs_concurrent_requests/httpclient.class.php
@@ -0,0 +1,40 @@
+<?php 
+
+abstract class HttpClient
+{
+	private static $_instance;
+	
+	function __construct()
+	{
+		throw new Exception('HttpClient is abstract, use static Create() method to obtain an instance');
+	}
+	
+	public static function Create()
+	{
+		if (HttpClient::_instance != null)
+		{
+			return HttpClient::$_instance;
+		}
+		
+		if (class_exists('http_class', false) && class_exists('sasl_interact_class', false)) {
+			HttpClient::$_instance = new PhpHttpClient(); 
+			return HttpClient::$_instance;
+		}
+		else if (function_exists('curl_init'))
+		{
+			HttpClient::$_instance = new CurlHttpClient();
+			return HttpClient::$_instance;
+		}
+		
+		throw new Exception('HttpClient could not find an underlying client library to use. Install Curl support or Manuel Lemos\' HttpClient');		
+	}
+	
+	public abstract function sendRequest($request);
+	public abstract function getResponseFor($request);
+	
+	//to add in later for non-blocking check of response readiness
+	//public abstract function checkResponseFor($request);
+}
+
+
+?>
