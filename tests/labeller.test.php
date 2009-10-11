64e50109b70b7a1fd0dd66a28e1c2d3e1329b8e4
Ian Davis <nospam@iandavis.com>
2009-10-11 20:02:48 +0000

    Added test case for labeller class get_label now attempts to split up camel cased local names for uris

---
+++ tests/labeller.test.php
@@ -0,0 +1,16 @@
+<?php
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'constants.inc.php';
+require_once MORIARTY_DIR . 'labeller.class.php';
+
+class LabellerTest extends PHPUnit_Framework_TestCase {
+
+  function test_get_label_splits_camelcase(){
+
+    $lab = new Labeller();
+    $this->assertEquals('school:RELIGIOUSCHARACTER', $lab->get_label('http://education.data.gov.uk/ontology/school#RELIGIOUSCHARACTER'));
+    $this->assertEquals('religious character', $lab->get_label('http://education.data.gov.uk/ontology/school#religiousCharacter'));
+  }
+
+
+}
+?>
