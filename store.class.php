640141098b9f89e90e3b2f9f808fbea29f717c6c
Keith Alexander <github@keithalexander.co.uk>
2011-05-18 12:57:43 +0000

    changed accept header of mirror_from_url

--- store.class.php
+++ store.class.php
@@ -230,6 +230,7 @@ class Store {
       
       $last_cached_page_uri = $this->get_contentbox()->uri.'/mirrors/'.$url;
       $web_page_request  = $this->request_factory->make('GET', $url); 
+      $web_page_request->set_accept('application/rdf+xml;q=0.8,text/turtle;q=0.9,*/*;q=0.1');
       $web_page_response = $web_page_request->execute();
       $return['get_page'] = $web_page_response;
       $web_page_content = $web_page_response->body;
